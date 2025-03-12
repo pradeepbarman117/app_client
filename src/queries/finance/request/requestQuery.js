008884d8743c5053616b3bbcd553fdf0f3ca7d66
Vishnu <srivis95@gmail.com>
2025-03-12 10:41:00 +0530

    Changes

--- src/queries/finance/request/requestQuery.js
+++ src/queries/finance/request/requestQuery.js
@@ -1,26 +1,3 @@
-// import { useQuery } from "@tanstack/react-query";
-// import requestServices from "../../../services/finance/request/requestServices";
-// import Cookies from 'js-cookie'; 
-
-// const fetchMasterRequest = async ({page})=>{
-//     try{
-//         const token = Cookies.get('token')
-//         const response = await requestServices.getMasterREQ(token,page);
-//         return response.data
-//     }catch(err){
-//         console.error(err,'error while fetching master request');
-//         throw new Error(err);
-//     }
-// }
-
-// export const useMasterRequestQuery = (page)=>{
-//     return useQuery({
-//         queryKey:['request/master'],
-//         queryFn:()=>fetchMasterRequest({page}),
-//     })
-// }
-
-
 import { useQuery } from "@tanstack/react-query";
 import requestServices from "../../../services/finance/request/requestServices";
 import Cookies from "js-cookie";
